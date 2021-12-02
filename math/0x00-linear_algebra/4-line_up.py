58b42ea8a50409f15a016d56000e6168570212c6
dontstopmeowing <dnny419@gmail.com>
2021-12-02 23:14:21 +0700

    Adding task 4.

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Contains the function matrix_shape """
+
+
+def add_arrays(arr1, arr2):
+    """ Function that adds two arrays element-wise"""
+
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        myList = []
+        for i in range(len(arr1)):
+            myList.append(arr1[i] + arr2[i])
+        return myList
