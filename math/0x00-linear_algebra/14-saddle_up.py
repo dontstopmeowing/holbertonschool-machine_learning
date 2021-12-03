5accd80f95fc9bbb44f249ed1b0a2c802b8c4697
dontstopmeowing <dnny419@gmail.com>
2021-12-03 08:06:30 +0700

    Adding task 14.

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Contains the function np_cat"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Function  that performs matrix multiplication"""
+
+    return np.matmul(mat1, mat2)
